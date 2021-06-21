acc382d7ab86ee4855c16111cfbe00c0f3c713b1
Mohd Saad <developedbysaad@gmail.com>
2021-06-21 19:55:12 +0530

    Todo Management CLI app

---
+++ assignment-3/todo.rb
@@ -0,0 +1,45 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.show_list
+    puts "My Todo-List\n\n\tOverdue\n\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\tDue Today\n\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\tDue Later\n\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.add_task(h)
+    Todo.create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete(id)
+    todo = Todo.find(id)
+    todo.completed = true
+    todo.save
+    todo
+  end
+end
