457e10f750bef77459d3dde0b08102966a0cb52e
Mohd Saad <developedbysaad@gmail.com>
2021-07-19 16:39:34 +0530

    refactor code in todo.rb

--- assignment-3/todo.rb
+++ assignment-3/todo.rb
@@ -23,13 +23,17 @@ class Todo < ActiveRecord::Base
     where("due_date > ?", Date.today)
   end
 
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+
   def self.show_list
     puts "My Todo-List\n\n\tOverdue\n\n"
-    puts overdue.map { |todo| todo.to_displayable_string }
+    puts overdue.to_displayable_list
     puts "\n\tDue Today\n\n"
-    puts due_today.map { |todo| todo.to_displayable_string }
+    puts due_today.to_displayable_list
     puts "\n\tDue Later\n\n"
-    puts due_later.map { |todo| todo.to_displayable_string }
+    puts due_later.to_displayable_list
   end
 
   def self.add_task(h)
