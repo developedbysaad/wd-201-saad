9304e64d0d577f405dfb5686a1b70ef8e1c67eb3
Mohd Saad <developedbysaad@gmail.com>
2021-09-30 07:25:47 +0530

    prefixed id to the todo list-item

--- assignment-3/todo.rb
+++ assignment-3/todo.rb
@@ -8,7 +8,7 @@ class Todo < ActiveRecord::Base
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
-    "#{display_status} #{todo_text} #{display_date}"
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
   def self.overdue
